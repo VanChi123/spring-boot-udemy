c4e1c32801e6bf932429708f2dab8213dae41963
chibv <chibv@fpt.com>
2024-06-23 23:10:22 +0700

    second commit in 01-ground-zero-branch

--- src/main/java/com/chibv/project1/Project1Application.java
+++ src/main/java/com/chibv/project1/Project1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Project1Application {
-
+	// second commit in 01
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
 	}
